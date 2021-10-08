7c1b07810bd89e5eecf110e75247b00b78420923
ETWilliams10 <williamsevan10@gmail.com>
2021-10-08 12:53:05 -0400

    added map feature layers and did some minimal styling

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,4 @@
-require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig,Map, MapView) {
+require(["esri/config","esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function (esriConfig,Map, MapView, FeatureLayer) {
     esriConfig.apiKey = "AAPKbbdca0354bd14e219ced6d2c9ff0409fm8qAWYWDXxZDafmWRXclQUUtOI03SgBdvMNUUo0tI8GQsXwEsBdGdNuR9edPlFc6";
     const map = new Map({
       basemap: "arcgis-topographic" // Basemap layer service
@@ -9,4 +9,18 @@ require(["esri/config","esri/Map", "esri/views/MapView"], function (esriConfig,M
         zoom: 13, // Zoom level
         container: "map" // Div element
     });
+    const trailheadsLayer = new FeatureLayer({
+      url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trailheads/FeatureServer/0"
+    });
+    map.add(trailheadsLayer);
+    //Trail Lines
+    const trailsLayer = new FeatureLayer({
+    url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Trails/FeatureServer/0"
+    });
+    map.add(trailsLayer, 0);
+    // Parks
+    const parksLayer = new FeatureLayer({
+      url: "https://services3.arcgis.com/GVgbJbqm8hXASVYi/arcgis/rest/services/Parks_and_Open_Space/FeatureServer/0"
+    });
+    map.add(parksLayer, 0);
 });
